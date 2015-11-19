20b6cabd7514f9857d1e54d5a8f1d7b3975e0804
Kiril Aleksandrov <kaleksandrov@users.noreply.github.com>
2015-11-19 17:24:28 +0200

    Update loggy.py

--- loggy.py
+++ loggy.py
@@ -8,7 +8,7 @@ from color import Color
 from threading import Thread
 
 #Set the filename and open the file
-filename = 'test.log'
+filename = '/var/log/vmware/vsan-health/vmware-vsan-health-service.log'
 
 def open_file(filename):
    file = open(filename,'r')
@@ -62,7 +62,7 @@ def print_file(file):
          file.seek(where)
       else:
          if is_new_log_entry(line):
-            if line.count(tag): 
+            if line.count(tag):
                color = get_line_color(line)
                LAST_COLOR = color
                println(line, color)
@@ -71,6 +71,33 @@ def print_file(file):
          elif LAST_COLOR:
             println(line, LAST_COLOR)
 
+class PrintThread(Thread):
+
+   def __init__(self, filename):
+      Thread.__init__(self)
+      self.filename = filename
+      self.kill_received = False
+      self.last_color = None
+
+   def run(self):
+      file = open_file(filename)
+      while not self.kill_received:
+         where = file.tell()
+         line = file.readline()
+
+         if not line:
+            time.sleep(1)
+            file.seek(where)
+         else:
+            if is_new_log_entry(line):
+               if line.count(tag):
+                  self.last_color = get_line_color(line)
+                  println(line, self.last_color)
+               else:
+                  self.last_color = None
+            elif self.last_color:
+               println(line, self.last_color)
+
 def get_command():
    global FLAG
    while FLAG:
@@ -78,17 +105,25 @@ def get_command():
       if line == 'exit':
          FLAG = False
 
+def signal_handler(signal, frame):
+   print 'CTRL+C'
+   global print_thread
+   print_thread.kill_received = True
+
+print_thread = None
+
 if __name__ == '__main__':
    print 'Hello!'
    print 'This is loggy :)'
    print "You are following: %s" % filename
 
-   file = open_file(filename)
-   t1 = Thread(target=print_file, args=(file,))
-   t1.start()
+   import signal
+   signal.signal(signal.SIGINT, signal_handler)
 
-   t2 = Thread(target=get_command)
-   t2.start()
-
-   t1.join()
-   t2.join()
+   global print_thread
+   while True:
+      print 'Starting new thread!'
+      print_thread = PrintThread(filename)
+      print_thread.start()
+      signal.pause()
+      print_thread.join()
