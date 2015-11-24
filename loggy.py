de858f2f90beed07edb83e2939a798f891c651e2
Kiril Aleksandrov <kaleksandrov@users.noreply.github.com>
2015-11-24 18:38:26 +0200

    Update loggy.py

--- loggy.py
+++ loggy.py
@@ -5,9 +5,7 @@ import sys
 import time
 from sys import stdout
 from color import Color
-
-#Set the filename and open the file
-FILENAME = '/var/log/vmware/vsan-health/vmware-vsan-health-service.log'
+from config import Configuration
 
 def open_file(filename):
    f = open(filename,'r')
@@ -16,42 +14,51 @@ def open_file(filename):
    f.seek(file_size)
    return f
 
-TYPE_ERROR = 0
-TYPE_INFO = 1
-TYPE_WARN = 2
+class LogProcessor(object):
 
-tag = 'VsanVcClusterConfigSystemImpl'
-ERROR_TOKEN = 'ERROR'
-INFO_TOKEN = 'INFO'
-WARN_TOKEN = 'WARN'
-DEBUG_TOKEN = 'DEBUG'
-CRITICAL_TOKEN = 'CRITICAL'
+   def __init__(self, config):
+      self.last_color = None
+      self.filter = config.filter
+      self.new_log_entry_regex = config.new_log_entry_regex
+      self.color_map = config.color_map.copy()
+      self.default_color = config.default_color
 
-FLAG = True
+   def get_line_color(self, line):
+      result = None
+      for entry, color in self.color_map.iteritems():
+         if line.count(entry):
+            result = color
+            break
+      else:
+         result = self.default_color
+
+      return result
+
+   def is_new_log_entry(self, line):
+      return line.startswith(self.new_log_entry_regex)
+
+   def println(self, line):
+      if self.is_new_log_entry(line):
+         if line.count(self.filter):
+            color = self.get_line_color(line)
+            self.last_color = color
+            self.print_with_color(line, color)
+         else:
+            self.last_color = None
+      elif self.last_color:
+         self.print_with_color(line, self.last_color)
+
+   @staticmethod
+   def print_with_color(line, color=Color.WHITE):
+      stdout.write("%s%s%s" % (color, line, Color.RESET))
 
 def is_new_line(value):
    return value == '' or value == '\r' or value == '\n' or value == '\r\n'
 
-def is_new_log_entry(line):
-   return line.startswith('2015')
+def print_file(f, log_processor, start_from=None):
+   if start_from:
+      f.seek(start_from)
 
-def get_line_color(line):
-   if line.count(ERROR_TOKEN):
-      return Color.RED
-   elif line.count(INFO_TOKEN):
-      return Color.WHITE
-   elif line.count(WARN_TOKEN):
-      return Color.YELLOW
-   elif line.count(DEBUG_TOKEN):
-      return Color.PURPLE
-   else:
-      return Color.WHITE
-
-def println(line, color=Color.WHITE):
-   stdout.write("%s%s%s" % (color, line, Color.RESET))
-
-def print_file(f):
-   LAST_COLOR = None
    while True:
       where = f.tell()
       line = f.readline()
@@ -60,22 +67,15 @@ def print_file(f):
          time.sleep(1)
          f.seek(where)
       else:
-         if is_new_log_entry(line):
-            if line.count(tag):
-               color = get_line_color(line)
-               LAST_COLOR = color
-               println(line, color)
-            else:
-               LAST_COLOR = None
-         elif LAST_COLOR:
-            println(line, LAST_COLOR)
+         log_processor.println(line)
 
-def mode_print():
+def mode_print(config):
    print 80*'-'
-   f = open_file(FILENAME)
-   print_file(f)
+   f = open_file(config.filename)
+   log_processor = LogProcessor(config)
+   print_file(f, log_processor)
 
-def mode_command():
+def mode_command(config):
    print
    print 80*'-'
    while True:
@@ -85,6 +85,10 @@ def mode_command():
          sys.exit(0)
       elif command == 'resume':
          return MODE_PRINT
+      elif command == 'config':
+         print config.filename
+         print config.filter
+         print config.new_log_entry_regex
       elif is_new_line(command):
          continue
       else:
@@ -95,20 +99,25 @@ def mode_command():
 MODE_PRINT = mode_print
 MODE_COMMAND = mode_command
 
+conf = Configuration()
+
 if __name__ == '__main__':
    print 'Hello!'
    print 'This is loggy :)'
-   print "You are following: %s" % FILENAME
+   print "You are following: %s" % conf.filename
 
    mode = MODE_PRINT
    while True:
       try:
-         mode = mode()
+         mode = mode(conf)
       except KeyboardInterrupt, kb:
          if mode == MODE_COMMAND:
             print
+            print 80*'-'
+            print 'Bye, bye! :)'
             sys.exit(0)
          else:
             mode = MODE_COMMAND
       except Exception, ex:
          print 'Something has gone wrong...'
+         print ex
