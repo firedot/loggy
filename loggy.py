7b831418b221d9f13f11545df777e6a9b5e23719
Kiril Aleksandrov <kaleksandrov@users.noreply.github.com>
2015-11-24 11:12:48 +0200

    Update loggy.py

--- loggy.py
+++ loggy.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python
 
 import os
+import sys
 import time
 from sys import stdout
 from color import Color
 
-from threading import Thread
-
 #Set the filename and open the file
-filename = 'test.log'
+FILENAME = '/var/log/vmware/vsan-health/vmware-vsan-health-service.log'
 
 def open_file(filename):
-   file = open(filename,'r')
+   f = open(filename,'r')
    file_stats = os.stat(filename)
    file_size = file_stats[6]
-   file.seek(file_size)
-   return file
+   f.seek(file_size)
+   return f
 
 TYPE_ERROR = 0
 TYPE_INFO = 1
@@ -30,7 +29,8 @@ CRITICAL_TOKEN = 'CRITICAL'
 
 FLAG = True
 
-LAST_COLOR = None
+def is_new_line(value):
+   return value == '' or value == '\r' or value == '\n' or value == '\r\n'
 
 def is_new_log_entry(line):
    return line.startswith('2015')
@@ -50,19 +50,18 @@ def get_line_color(line):
 def println(line, color=Color.WHITE):
    stdout.write("%s%s%s" % (color, line, Color.RESET))
 
-def print_file(file):
-   global FLAG
-   global LAST_COLOR
-   while FLAG:
-      where = file.tell()
-      line = file.readline()
+def print_file(f):
+   LAST_COLOR = None
+   while True:
+      where = f.tell()
+      line = f.readline()
 
       if not line:
          time.sleep(1)
-         file.seek(where)
+         f.seek(where)
       else:
          if is_new_log_entry(line):
-            if line.count(tag): 
+            if line.count(tag):
                color = get_line_color(line)
                LAST_COLOR = color
                println(line, color)
@@ -71,24 +70,51 @@ def print_file(file):
          elif LAST_COLOR:
             println(line, LAST_COLOR)
 
-def get_command():
-   global FLAG
-   while FLAG:
-      line = raw_input()
-      if line == 'exit':
-         FLAG = False
+def mode_print():
+   print
+   print '=================================='
+   print ' You have entered print mode'
+   print '=================================='
+   f = open_file(FILENAME)
+   print_file(f)
+
+def mode_command():
+   print
+   print '=================================='
+   print ' You have entered command mode'
+   print '=================================='
+   while True:
+      stdout.write('[command]: ')
+      command = raw_input()
+      if command == 'exit':
+         sys.exit(0)
+      elif command == 'resume':
+         return MODE_PRINT
+      elif is_new_line(command):
+         continue
+      else:
+         print "\tUnknown command: %s" % command
+
+   return None
+
+MODE_PRINT = mode_print
+MODE_COMMAND = mode_command
 
 if __name__ == '__main__':
    print 'Hello!'
    print 'This is loggy :)'
-   print "You are following: %s" % filename
+   print "You are following: %s" % FILENAME
 
-   file = open_file(filename)
-   t1 = Thread(target=print_file, args=(file,))
-   t1.start()
-
-   t2 = Thread(target=get_command)
-   t2.start()
-
-   t1.join()
-   t2.join()
+   global_stop = False
+   mode = MODE_PRINT
+   while not global_stop:
+      try:
+         mode = mode()
+      except KeyboardInterrupt, kb:
+         if mode == MODE_COMMAND:
+            print
+            sys.exit(0)
+         else:
+            mode = MODE_COMMAND
+      except Exception, ex:
+         print 'Something has gone wrong...'
